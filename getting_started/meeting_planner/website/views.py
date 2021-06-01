d2eaeed716ad336e32298445dfba49bc85b714bd
randle9000 <randle9000@gmail.com>
2021-06-01 17:58:55 +0200

    urls and views updates

--- getting_started/meeting_planner/website/views.py
+++ getting_started/meeting_planner/website/views.py
@@ -1,3 +1,12 @@
+import datetime
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse("Welcome to the metting planer")
+
+def date(request):
+    return HttpResponse("this page was server at " + str(datetime.datetime.now()))
+
