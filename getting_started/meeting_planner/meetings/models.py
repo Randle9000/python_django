0a2f5360b4a0bec21c002e51e66de8973d1717b5
randle9000 <randle9000@gmail.com>
2021-06-01 18:45:18 +0200

    meeting app added to admin, create model related to meetings planning, app added to settings

--- getting_started/meeting_planner/meetings/models.py
+++ getting_started/meeting_planner/meetings/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+# represent table in db (obejts are the rows)
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
