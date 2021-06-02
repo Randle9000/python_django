132e53c187530d4cb821556db1803e8b7b5d67c4
randle9000 <randle9000@gmail.com>
2021-06-02 12:42:26 +0200

    first Model view implementation

--- getting_started/meeting_planner/website/views.py
+++ getting_started/meeting_planner/website/views.py
@@ -2,11 +2,15 @@ import datetime
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meeting
 
 # Create your views here.
 def welcome(request):
     return render(request, 'website/welcome.html',
-                  {'message': 'this data is from def welcome'})
+                  {'message': 'template variable this data is from def welcome',
+                   'x': 42,
+                   'num_meetings': Meeting.objects.count()}
+                  )
 
 
 def date(request):
