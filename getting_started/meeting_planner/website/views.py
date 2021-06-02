87f0758b656ee98f6aecfb54820aeed2de017d96
randle9000 <randle9000@gmail.com>
2021-06-02 12:06:11 +0200

    added templates for website app and use render instead http response in views of website app

--- getting_started/meeting_planner/website/views.py
+++ getting_started/meeting_planner/website/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the metting planer")
+    return render(request, 'website/welcome.html')
+
 
 def date(request):
     return HttpResponse("this page was server at " + str(datetime.datetime.now()))
