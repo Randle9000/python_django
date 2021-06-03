353cbdf0053d51cbde03cae4945b816f862a6888
randle9000 <randle9000@gmail.com>
2021-06-03 13:49:01 +0200

    generated form using modelform_factory and based on model (in that case meeting) EXAMPLE

--- getting_started/meeting_planner/meetings/urls.py
+++ getting_started/meeting_planner/meetings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<int:id>', views.details, name='detail'),
-    path("rooms", views.rooms_details, name='room_details')
+    path("rooms", views.rooms_details, name='room_details'),
+    path("new", views.new, name='new')
 ]
--- getting_started/meeting_planner/meetings/views.py
+++ getting_started/meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
 
 from .models import Meeting, Room
 
@@ -6,5 +7,16 @@ def details(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {'meeting': meeting})
 
+
 def rooms_details(request):
-    return render(request, 'meetings/rooms_list.html', {'rooms': Room.objects.all()})
+    return render(request, 'meetings/rooms_list.html',
+                  {'rooms': Room.objects.all()})
+
+
+MeetingForm = modelform_factory(Meeting, exclude=[]) # MeetingFrom is a class not a regula object!!
+
+
+def new(request):
+    form = MeetingForm() # create object
+    return render(request, 'meetings/new.html',
+                  {'form': form})
