1302a54b80acebd675529bf6b5cf94078811f668
randle9000 <randle9000@gmail.com>
2021-06-03 12:37:13 +0200

    fix type in urls.py in meetings

--- getting_started/meeting_planner/meetings/urls.py
+++ getting_started/meeting_planner/meetings/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>', views.detail, name='detail'),
+    path('<int:id>', views.details, name='detail'),
     path("rooms", views.rooms_details, name='room_details')
 ]
